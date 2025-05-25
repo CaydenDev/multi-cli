038f2430aaf0e086aa744f9bb53f1216ce23c8c4
CaydenDev <c@lincke.de>
2025-05-25 10:49:44 +0200

    Version Update

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ const program = new Command();
 program
   .name('mcli')
   .description('A user-friendly multi-purpose CLI tool')
-  .version('MCLI 1.1.0');
+  .version('MCLI 1.1.2');
 
 // Register all commands
 apiTest(program);
